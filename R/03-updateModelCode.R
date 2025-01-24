c720936629e8ca7afc1cf35683f5d3fa265e604b
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-01-24 09:24:08 +0100

    add sanitizeInput function

--- R/03-updateModelCode.R
+++ R/03-updateModelCode.R
@@ -61,19 +61,19 @@ updateModelCode <- function(fruitsObj, newModelCode, newModelInputs = NULL) {
   
   if (length(fruitsObj) == 0 ||
       length(newModelCode) == 0 || newModelCode == "" ||
-      length(newModelInputs) == 0 || newModelInputs == "") {
+      length(newModelInputs) == 0) {
     # nothing to update
     return(fruitsObj)
   }
   
   # update model inputs in fruits object
   for (name in names(newModelInputs)) {
-    fruitsObj[[name]] <- eval(parse(text = newModelInputs[[name]]))
+    fruitsObj[[name]] <- eval(parse(text = sanitizeInput(newModelInputs[[name]])))
   }
   
   # update 'modelCode' in fruits object
   # Evaluate the (new) nimbleCode string, then this corresponds to the output of tmplEval() in the createModelCode() function
-  evaluated_code <- eval(parse(text = paste("nimbleCode(", newModelCode, ")")))
+  evaluated_code <- eval(parse(text = paste("nimbleCode(", sanitizeInput(newModelCode), ")")))
   
   # Explicitly assign the evaluated code to the object
   fruitsObj$modelCode <- evaluated_code
@@ -84,4 +84,40 @@ updateModelCode <- function(fruitsObj, newModelCode, newModelInputs = NULL) {
 
 isRunningOnline <- function() {
   as.logical(Sys.getenv("SHINYPROXY") != "", unset = "FALSE")
+}
+
+sanitizeInput <- function(input) {
+  # Check for empty input explicitly (valid case)
+  if (input == "") {
+    return(input)
+  }
+  
+  basic_regex <- "^[[:print:][:space:]]+$"
+  
+  # Step 1: Basic validation to check for printable characters and spaces
+  is_valid <- grepl(basic_regex, input)
+  if (!is_valid) {
+    stop("Invalid input detected: contains non-printable or disallowed characters.")
+  }
+  
+  # Step 2: Check for explicitly forbidden patterns
+  forbidden_patterns <- forbidden_patterns <- c(
+    "system\\(", "system2\\(", "shell\\(",
+    "eval\\(", "parse\\(", "source\\(", "do\\.call\\(",
+    "file\\(", "unlink\\(", "dir\\(", "list\\.files\\(",
+    "read\\.table\\(", "read\\.csv\\(", "write\\.csv\\(",
+    "get\\(", "assign\\(", "environment\\(",
+    "library\\(", "require\\(",
+    "Sys\\.setenv\\(", "Sys\\.getenv\\(",
+    "options\\(",
+    "proc\\.time\\(", "pskill\\(", "system\\.time\\(",
+    "download\\.file\\(", "url\\(", "getURL\\(", "readLines\\(",
+    "httr::GET\\(", "httr::POST\\("
+  )
+  
+  if (any(sapply(forbidden_patterns, grepl, input))) {
+    stop("Invalid input detected: contains forbidden patterns.")
+  }
+  
+  input
 }
